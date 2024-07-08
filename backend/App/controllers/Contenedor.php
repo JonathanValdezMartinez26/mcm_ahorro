d1d0a5dd697481e96569a3141efc516ea9742b23
soporteMCM <alberto.s@masconmenos.com.mx>
2024-07-08 14:45:57 -0600

    Corrección

--- backend/App/controllers/Contenedor.php
+++ backend/App/controllers/Contenedor.php
@@ -334,7 +334,23 @@ html;
                 <div class="nav_menu">
                     <nav>
                         <div class="nav toggle">
-       }
+                            <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                        </div>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="">
+                                <a href="" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                    <span class=" fa fa-user"></span> {$nombre}
+                                    <span class=" fa fa-angle-down"></span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-usermenu pull-right">
+                                    <li><a href="/Login/cerrarSession"><i class="fa fa-sign-out pull-right"></i>Cerrar Sesión</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
         html;
 
         return $header . $menu;
